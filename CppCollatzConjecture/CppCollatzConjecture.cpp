aa842d58fb65680fca11d8fc570ace18e0366b9e
Will S <fevenkitsune@gmail.com>
2021-11-06 13:53:36 -0400

    Add project files.

---
+++ CppCollatzConjecture/CppCollatzConjecture.cpp
@@ -0,0 +1,12 @@
+﻿// CppCollatzConjecture.cpp : Defines the entry point for the application.
+//
+
+#include "CppCollatzConjecture.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
